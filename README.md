ac6773502d454d0d6ae2baab0c4812449d9d67d7
zack <shiqizhenyes@gmail.com>
2020-06-06 16:16:12 +0800

    优化首页样式
    优化首页样式

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Flutter widget of the week
 
+## Main Page
+
+<img src="img/index.png" width = "200" height = "100%" alt="" align=center />
+
 ## AboutDialog
 
 <img src="img/about_dialog.png" width = "200" height = "100%" alt="" align=center />
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:WOW/AboutDialogDemo.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         primaryColorLight: Colors.white,
         primaryColorDark: Colors.black,
-        brightness: Theme.of(context).brightness,
+        brightness: Brightness.light,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter WOW'),
@@ -34,11 +36,38 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
+  var widgetImages = <String>[
+    "images/index.webp",
+    "images/widget1.webp",
+    "images/widget2.webp",
+    "images/widget83.webp",
+    ];
+
+  var titles = <String>[
+    "Introducing Widget of the Week!",
+    "SafeArea",
+    "Expanded",
+    "AboutDialog"
+  ];
+
+  var subtitles = <String>[
+    "Flutter",
+    "Google developments"
+  ];
+
+  _onPressed(int index) {
+    switch (index) {
+      case 3:
+        _goToAboutDialog();
+        break;
+      default:
+    }
+  }
+
   _goToAboutDialog() {
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return AboutDialogDemo();
     }));
-    
   }
 
   @override
@@ -48,11 +77,43 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(onPressed: _goToAboutDialog, child: Text("AboutDialog")),
-          ],
+        child: Scrollbar(
+          child: ListView.builder(
+            itemBuilder: (context, int index) {
+              return FlatButton(
+                onPressed: () {
+                  _onPressed(index);
+                },
+                padding: EdgeInsets.all(0.0), 
+                child: Container(
+                  padding: EdgeInsets.only(left: 8.0, top: 10, right: 16.0, bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 8.0),
+                        child: Text((1 + index).toString(), style: TextStyle(fontSize: 14, color: Colors.grey),),
+                      ),
+                      Image.asset(this.widgetImages[index], width: 120, fit: BoxFit.fitWidth,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: 16.0),
+                            child: Text(this.titles[index], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 16.0),
+                            child: Text(this.subtitles[0], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 8),),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              );  
+            },
+            itemCount: widgetImages.length,
+          )
         ),
       ),
     );
